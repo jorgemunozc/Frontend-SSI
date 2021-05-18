3ed22e47bfd9b84c31ceed91491abbdedc9f2377
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-05-18 14:30:38 -0400

    Componentes para el Formulario 29

---
+++ src/components/F29/DebitosYVentas.vue
@@ -0,0 +1,32 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>IMPUESTO AL VALOR AGREGADO D.L 825/74</th>
+                <th>Cantidad de Documentos</th>
+                <th>Monto Neto</th>
+            </tr>
+        </thead>
+        <!-- Cuerpo de tabla 9 columnas de ancho -->
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Ventas y/o Servicios Prestados</td>
+                <td>Informaci&oacute;n de Ingresos</td>
+                <td>Exportaciones</td>
+                <td>585</td>
+                <td></td>
+                <td>20</td>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Ventas y/o Servicios prestados Exentos, o No Gravados del giro</td>
+                <td>586</td>
+                <td></td>
+                <td>142</td>
+                <td colspan="2"></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
