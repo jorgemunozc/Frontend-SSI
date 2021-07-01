693c618fd9419c568a25de8edc57d033309763fb
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-07-01 12:49:27 -0400

    agregado servicio para acceder a API de f29

---
+++ src/services/F29Service.ts
@@ -0,0 +1,53 @@
+import { get, post, update } from '@/services/ApiService';
+
+
+interface F29Response extends Formulario29 {
+  folio: number,
+  periodo: string,
+}
+
+interface Periodo {
+  month: number,
+  year: number,
+}
+
+const resource = 'f29';
+
+function obtenerF29(folio?: number): Promise<F29Response> {
+  return get(resource, folio);
+}
+
+function guardarF29(data: Formulario29, estado: string, periodo?: string) {
+  let payload = {};
+  if (periodo) {
+    payload = {...data, 'estado': estado, 'periodo': periodo}
+  } else {
+    payload = {...data, 'estado': estado }; 
+  }
+  return post<Object>(resource, payload);
+}
+
+function actualizarF29(data: Formulario29, folio: number, nuevoEstado?: string) {
+  let payload = {...data};
+  if (typeof nuevoEstado != 'undefined') {
+    payload = {...data, 'estado': nuevoEstado};
+  }
+  return update(resource, folio, payload);
+}
+
+function buscarF29(periodo: Periodo): Promise<F29Response> {
+  const campoBusqueda = 'periodo';
+  return post('buscar-f29-por', {...periodo, field: campoBusqueda});
+}
+
+function obtenerFormularios(): Promise<Object> {
+  return get('f29');
+}
+
+export {
+  obtenerF29,
+  guardarF29,
+  actualizarF29,
+  buscarF29,
+  obtenerFormularios,
+}
