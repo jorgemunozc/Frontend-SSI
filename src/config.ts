d8ba73d0925a3d8df3bf63844bcc38799445ce5c
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-07-03 18:35:33 -0400

    Agregadas rutas de produccion en comentarios

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
-const API_HOST = 'http://localhost:8080/sii/api';
-const SERVER_HOST = 'http://localhost:8080/sii';
+const API_HOST = 
+'http://localhost:8080/sii/api';
+// 'https://ssi-api.feriasimulada.cl/api'; 
+const SERVER_HOST = 
+'http://localhost:8080/sii';
+// 'https://ssi-api.feriasimulada.cl'; 
 
 export {API_HOST, SERVER_HOST};
