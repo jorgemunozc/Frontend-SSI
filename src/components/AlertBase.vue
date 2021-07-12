2dcf662122da5fea0f540452e0321a6169793afe
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-07-12 18:09:28 -0400

    Agregados nuevos componentes

---
+++ src/components/AlertBase.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="alert-footer container fixed bottom-0">
+    <input :value="isOpen" @input="$emit('update:isOpen')" type="checkbox" id="footeralert" class="hidden" />
+    <div
+      class="
+        flex
+        items-center
+        justify-between
+        w-full
+        p-2
+        bg-red-500
+        shadow
+        text-white
+      "
+    >
+      <span class="flex-grow text-center">{{ mensaje }}</span>
+      <label class="close cursor-pointer" title="close" for="footeralert">
+        <svg
+          class="fill-current text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+        >
+          <path
+            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+          ></path>
+        </svg>
+      </label>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    mensaje: String,
+    isOpen: Boolean,
+  },
+  emits: ['update:isOpen'],
+});
+</script>
