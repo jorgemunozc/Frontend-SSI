3e0f1cb05e855392c885828540f582e6a389a622
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-07-20 12:40:47 -0400

    agregada store para dominios y nuevas funciones CRUD en servicio

--- src/services/DominioService.ts
+++ src/services/DominioService.ts
@@ -1,10 +1,17 @@
-import { get } from "@/services/ApiService";
+import { get, post, deleteResource } from "@/services/ApiService";
 
 const resource = 'dominios';
+
 function getDominios() {
-    return get<DominioCorreo>(resource).then(res => res);
-    // return get<string>('hola');
+    return get<DominioCorreo[]>(resource).then(res => res);
 }
 
+function agregarDominio(dominio: string) {
+    return post<DominioCorreo>(resource, { 'dominio': dominio });
+}
 
-export {getDominios};
+function eliminarDominio(id: number) {
+    return deleteResource(resource, id);
+}
+
+export { getDominios, agregarDominio, eliminarDominio };
---
+++ src/store/dominios.module.ts
@@ -0,0 +1,29 @@
+import { reactive } from "vue";
+
+const store = {
+  state: reactive({
+    dominios: [] as DominioCorreo[],
+  }),
+
+  agregarDominio(dominio: DominioCorreo): boolean {
+    try {
+      this.state.dominios.push(dominio)
+    } catch (error) {
+      return false;
+    }
+    return true;
+  },
+  eliminarDominio(id: number) {
+    for (let i = 0; i < this.state.dominios.length; i++) {
+      const currId = this.state.dominios[i].id;
+      if (currId === id) {
+        this.state.dominios.splice(i, 1)
+      }
+    }
+  },
+  vaciarLista() {
+    this.state.dominios.length = 0;
+  }
+}
+
+export default store;
