c3f6278e04a3429fc7563af8a2849be8254e7e21
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-10-27 16:58:14 -0300

    Modificacion de rutas en API

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -2,15 +2,17 @@ import { post } from '@/services/ApiService';
 import axios from 'axios';
 import { SERVER_HOST } from '../config';
 import { removeAdmin, removeLoggedIn } from '@/auth/unsetAuth';
+
+
 function auth(credentials: object) {
     const endpoint = 'login';
     return axios.get(`${SERVER_HOST}/sanctum/csrf-cookie`).then(() =>
-        post<Object>(endpoint, credentials)
+        post<Object>(`${SERVER_HOST}/${endpoint}`, credentials)
     )
 }
 
 function logout()  {
-    return post<Object>('logout', {}).then(() => {
+    return post<Object>(`${SERVER_HOST}/logout`, {}).then(() => {
         removeLoggedIn();
         removeAdmin();
     })
