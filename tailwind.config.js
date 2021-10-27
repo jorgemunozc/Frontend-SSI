51cf4e4907190d937f38e6bec3bbe887cca56532
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-10-27 16:46:47 -0300

    agregado valores personalizados a tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '60': '.60',
+        '70': '.70'
+      },
+      minWidth: {
+        '1': '1px'
+      }
+    },
   },
   variants: {
     extend: {},
