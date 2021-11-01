385486d334cd9a98e4f3e39afeb05f4c0c732cce
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-01 20:35:45 -0300

    Componente muestra una vista dependiendo del tipo de usuario

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -2,11 +2,18 @@
   <div class="container overflow-hidden">
     <Navbar class="md:h-full" />
     <div class="flex flex-col md:ml-48 p-2 h-full">
-      <h1 class="text-blue-700 font-semibold text-2xl mb-4">
-        Gesti&oacute;n de Solicitudes de Ingreso a Plataforma
-      </h1>
-      <div class="overflow-y-auto">
-        <SolicitudList />
+      <template v-if="hasAdminPrivileges">
+        <h1 class="text-blue-700 font-semibold text-2xl mb-4">
+          Gesti&oacute;n de Solicitudes de Ingreso a Plataforma
+        </h1>
+        <div class="overflow-y-auto">
+          <SolicitudList />
+        </div>
+      </template>
+      <div v-else>
+        <h1 class="text-blue-700 font-semibold text-4xl mx-auto max-w-xl text-center">
+          Bienvenido a la plataforma de Servicio Simulado de Impuestos
+        </h1>
       </div>
     </div>
   </div>
@@ -15,10 +22,17 @@
 import { defineComponent } from "vue";
 import Navbar from "@/components/Navbar.vue";
 import SolicitudList from "@/components/solicitudes/SolicitudesList.vue";
+import isAdmin from "@/auth/isAdmin";
 export default defineComponent({
   components: {
     Navbar,
     SolicitudList,
   },
+  setup() {
+    const hasAdminPrivileges = isAdmin();
+    return {
+      hasAdminPrivileges
+    }
+  }
 });
 </script>
